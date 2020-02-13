d1f518e19ce5f01024b0b2e10dcdf19317b78d71
nzhfeldman <nzhfeldman@gwu.edu>
2020-02-12 21:31:10 -0500

    Add Data and original RMDs

---
+++ CollateData.R
@@ -0,0 +1,3 @@
+library(readxl)
+freedom_raw <- read_excel("Freedom_In_The_50_States_2018.xlsx", 
+                                            sheet = "Overall")
---
+++ Links.R
@@ -0,0 +1,4 @@
+# To Do List: https://docs.google.com/document/d/1GcHF-gP61e2c3TsAeVcJbtxoiGZytwC-SJbUv_jtcuU/edit?usp=sharing
+
+# Data Source:
+
